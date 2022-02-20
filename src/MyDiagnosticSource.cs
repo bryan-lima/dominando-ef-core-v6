7b5deb1034f715060b3a39d437f3aebe51bb390d
Bryan <bryanlima200@outlook.com>
2022-02-20 19:16:18 -0300

    Adicionado e confgurado um interceptador

---
+++ src/MyDiagnosticSource.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EFCore
+{
+    public class MyInterceptor : IObserver<KeyValuePair<string, object>>
+    {
+        private static readonly Regex _tableAliasRegex = new Regex(@"(?<tableAlias>FROM +(\[.*\]\.)?(\[.*\]) AS (\[.*\])(?! WITH \(NOLOCK\)))",
+                                                                   RegexOptions.Multiline | RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+        public void OnCompleted()
+        {
+            
+        }
+
+        public void OnError(Exception error)
+        {
+            
+        }
+
+        public void OnNext(KeyValuePair<string, object> value)
+        {
+            if (value.Key == RelationalEventId.CommandExecuting.Name)
+            {
+                var command = ((CommandEventData)value.Value).Command;
+
+                if (!command.CommandText.Contains("WITH (NOLOCK)"))
+                {
+                    command.CommandText = _tableAliasRegex.Replace(command.CommandText, "${tableAlias} WITH (NOLOCK)");
+                }
+            }
+        }
+    }
+}
